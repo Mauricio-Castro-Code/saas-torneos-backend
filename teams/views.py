9263259603dcd3764e86fb797cd24b0e309b637d
Basstiida <rios.jorge.sec280@gmail.com>
2026-07-29 23:08:46 -0600

    feat: permission EsCapitanDeSuEquipo y endpoint mi_equipo

--- teams/views.py
+++ teams/views.py
@@ -8,8 +8,8 @@ from leagues.models import Liga
 from leagues.permissions import EsAdmin
 
 from .models import Equipo, Jugador
-from .permissions import EsJugador
-from .serializers import EquipoPublicoSerializer, EquipoSerializer
+from .permissions import EsCapitanDeSuEquipo, EsJugador
+from .serializers import EquipoCapitanSerializer, EquipoPublicoSerializer, EquipoSerializer
 
 
 class EquipoViewSet(viewsets.ModelViewSet):
@@ -78,3 +78,28 @@ class EquipoViewSet(viewsets.ModelViewSet):
             },
             status=status.HTTP_201_CREATED,
         )
+
+    @action(
+        detail=False,
+        methods=["get", "patch"],
+        url_path="mi_equipo",
+        permission_classes=[IsAuthenticated, EsCapitanDeSuEquipo],
+    )
+    def mi_equipo(self, request):
+        try:
+            equipo = Equipo.objects.get(capitan=request.user)
+        except Equipo.DoesNotExist:
+            return Response(
+                {"detail": "No eres capitán de ningún equipo."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        self.check_object_permissions(request, equipo)
+
+        if request.method == "GET":
+            return Response(EquipoCapitanSerializer(equipo).data)
+
+        serializer = EquipoCapitanSerializer(equipo, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
